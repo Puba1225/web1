78ac843e7a7ffdc3a9a4637e0258c1ccb8ff0520
Puba1225 <93768369+Puba1225@users.noreply.github.com>
2021-11-05 04:59:27 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css" />
+<body>
+	<header class="header">
+		<div class="logo"> <img src="ajillii.png" alt="logo baihgui" height="50px" width=250px> </div>
+		<div class="main" >
+			<ul>
+				<li><a href="ajliin bairuud" > Ажлын байрууд    </a> </li>
+				<li><a href="newtreh" > Нэвтрэх                 </a> </li>
+				<li><a href="burtguuleh" > Бүртгүүлэх           </a> </li>
+			</ul>
+		</div>
+	</header>
+	<div> 
+		<P> Танд ажилчин хэрэгтэй байна уу? <br> 
+			Та ажлын хажуугаараа орлоготой болмоор байна? <br>
+			Шаргуу хөдөлмөрын үр дүн нь АМЖИЛТАНД ХҮРЭХ <br>
+			Зогсолтгүй ХӨДӨЛМӨРӨЛ 
+		</P>
+	</div>
+	<main>
+		<div> <a href="tsagiin-ajil">Цагийн ажил</a>          </div>
+		<div> <a href="tsagiin-ajil">Хүргэлт</a>              </div>
+		<div> <a href="tsagiin-ajil">Борлуулалт</a>           </div>    
+		<div> <a href="tsagiin-ajil">Амрагч Үйлчлэгч </a>     </div>      
+		<div> <a href="tsagiin-ajil">Зөөгч Угаагч</a>         </div>       
+		<div> <a href="tsagiin-ajil">Худалдагч Касс</a>       </div>        
+		<div> <a href="tsagiin-ajil">Ачих Буулгах</a>         </div>        
+		<div> <a href="tsagiin-ajil">Барилга </a>             </div>     
+		<div> <a href="tsagiin-ajil">Зам анги </a>            </div>      
+		<div> <a href="tsagiin-ajil">Харуул хамгаалалт </a>   </div>
+		
+    </main>
+	<main2 class="aimag">
+		<div> <a href="tsagiin-ajil"> Улаанбаатар	  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Архангай  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Баян-Өлгий  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Баянхонгор  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Булган  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Говь-Алтай </a>           </div>
+		<div> <a href="tsagiin-ajil"> Говьсүмбэр  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Дархан-Уул  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Дорноговь  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Дорнод	  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Дундговь </a>           </div>
+		<div> <a href="tsagiin-ajil"> Завхан </a>           </div>
+		<div> <a href="tsagiin-ajil"> Орхон  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Өвөрхангай  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Өмнөговь  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Сүхбаатар  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Сэлэнгэ  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Төв </a>           </div>
+		<div> <a href="tsagiin-ajil"> Увс  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Ховд  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Хөвсгөл  </a>           </div>
+		<div> <a href="tsagiin-ajil"> Хэнтий  </a>           </div>
+    </main>
+	
+</body>
+</html>
